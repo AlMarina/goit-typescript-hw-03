706829764709a93c53bec65f85c48d22f486deee
AlMarina <almarina1017@gmail.com>
2023-12-10 03:29:51 +0200

    end

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,41 @@
+class Key {
+  constructor(private signature: number = Math.random()) {}
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  private tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  comeIn(person: Person) {
+    if (this.door) this.tenants.push(person);
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key) {
+    if (key.getSignature() === this.key.getSignature()) this.door = true;
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +44,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
